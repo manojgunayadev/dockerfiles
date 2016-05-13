fecc402fc7be5f56010af463df9f49d3a618c07f
manoj <manojg@wwso2.com>
2016-05-13 12:20:07 +0530

    config changes

--- apim-distributed/scripts/run.sh
+++ apim-distributed/scripts/run.sh
@@ -1,55 +1,30 @@
 ##
-
 ## Create run.sh file with following content and save it in same folder as the above
-
 ## set_root_pw.sh
-
 ##
 
 #!/bin/bash
 
 if [ "${AUTHORIZED_KEYS}" != "**None**" ]; then
-
-    echo "=> Found authorized keys"
-
+    echo "=&gt; Found authorized keys"
     mkdir -p /root/.ssh
-
     chmod 700 /root/.ssh
-
     touch /root/.ssh/authorized_keys
-
     chmod 600 /root/.ssh/authorized_keys
-
-    IFS=
-
-\n'
-
+    IFS=$'\n'
     arr=$(echo ${AUTHORIZED_KEYS} | tr "," "\n")
-
     for x in $arr
-
     do
-
         x=$(echo $x |sed -e 's/^ *//' -e 's/ *$//')
-
-        cat /root/.ssh/authorized_keys | grep "$x" >/dev/null 2>&1
-
+        cat /root/.ssh/authorized_keys | grep "$x" &gt;/dev/null 2&gt;&amp;1
         if [ $? -ne 0 ]; then
-
-            echo "=> Adding public key to /root/.ssh/authorized_keys: $x"
-
-            echo "$x" >> /root/.ssh/authorized_keys
-
+            echo "=&gt; Adding public key to /root/.ssh/authorized_keys: $x"
+            echo "$x" &gt;&gt; /root/.ssh/authorized_keys
         fi
-
     done
-
 fi
 
 if [ ! -f /.root_pw_set ]; then
-
 /set_root_pw.sh
-
 fi
-
 exec /usr/sbin/sshd -D
--- apim-distributed/scripts/set_root_pw.sh
+++ apim-distributed/scripts/set_root_pw.sh
@@ -1,43 +1,26 @@
 ##
-
 ## Create Set Root Password Script. Name it as set_root_pw.sh. Save it in a folder
-
 ##
 
 #!/bin/bash
-
 if [ -f /.root_pw_set ]; then
-
 echo "Root password already set!"
-
 exit 0
-
 fi
 
 PASS=${ROOT_PASS:-$(pwgen -s 12 1)}
-
-_word=$( [ ${ROOT_PASS} ] && echo "preset" || echo "random" )
-
-echo "=> Setting a ${_word} password to the root user"
-
+_word=$( [ ${ROOT_PASS} ] &amp;&amp; echo "preset" || echo "random" )
+echo "=&gt; Setting a ${_word} password to the root user"
 echo "root:$PASS" | chpasswd
 
-echo "=> Done!"
-
+echo "=&gt; Done!"
 touch /.root_pw_set
 
 echo "========================================================================"
-
 echo "You can now connect to this CentOS container via SSH using:"
-
 echo ""
-
 echo "    ssh -p  root@"
-
 echo "and enter the root password '$PASS' when prompted"
-
 echo ""
-
 echo "Please remember to change the above password as soon as possible!"
-
 echo "========================================================================"
