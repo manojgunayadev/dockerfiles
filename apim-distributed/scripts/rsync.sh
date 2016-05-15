8d7250097a478ef6fde97659ef74def6b4197761
manoj <manojg@wwso2.com>
2016-05-15 09:13:57 +0530

    config changes

--- apim-distributed/scripts/rsync.sh
+++ apim-distributed/scripts/rsync.sh
@@ -2,4 +2,9 @@
 mkdir ~/.ssh
 cd ~/.ssh
 ssh-keygen -f verizon.rsa -t rsa -N ''
-sshpass -p "screencast" rsync -avz -e ssh -p 31222" root@mgtgw1.marathon.slave.mesos.:/mnt/wso2/wso2am-1.10.0/repository/deployment/server/synapse-configs /mnt/wso2/wso2am-1.10.0/repository/deployment/server/
+sshpass -p "screencast" ssh-copy-id -o "StrictHostKeyChecking no" -i ~/.ssh/verizon.rsa.pub mgtgw1.marathon.slave.mesos. -p 31222
+while true 
+do 
+sshpass -p "screencast" rsync -e 'ssh -p 31222' -avzp mgtgw1.marathon.slave.mesos.:/mnt/wso2/wso2am-1.10.0/repository/logs/ /mnt/wso2/
+sleep 10
+done
